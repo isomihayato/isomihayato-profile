1af6627c0d40bb9739fc6425b3384f5fe9ef78f7
isomihayato <isomihayato@gmail.com>
2026-08-05 17:48:30 +0900

    ちらつき緩和

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,9 +7,12 @@
     <meta name="description" content="フルスタックエンジニア ISHIDA TOMOYA のポートフォリオサイト">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=VT323&display=swap" rel="stylesheet">
+    <link rel="preload" href="/fonts/Perfect%20DOS%20VGA%20437.ttf" as="font" type="font/ttf" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=VT323&display=block" rel="stylesheet">
     <title>ISHIDA TOMOYA — Fullstack Engineer</title>
+    <style>
+        html, body { background: #010800; color: rgb(57, 255, 20); }
+    </style>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
